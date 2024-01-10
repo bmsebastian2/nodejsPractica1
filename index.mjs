69c3596bab13b8edb346b7292f991a4188879b59
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2024-01-10 15:24:44 -0300

    Primer commit

---
+++ index.mjs
@@ -0,0 +1,4 @@
+import { suma, resta, saludo } from "./suma.mjs";
+console.log(suma(5, 3));
+console.log(resta(10, 10));
+console.log(saludo("mam"));
---
+++ suma.mjs
@@ -0,0 +1,10 @@
+export function suma(a, b) {
+  return a + b;
+}
+export function resta(a, b) {
+  return a - b;
+}
+
+export const saludo = (name) => console.log("Hello", name);
+
+export default { suma, resta, saludo };
